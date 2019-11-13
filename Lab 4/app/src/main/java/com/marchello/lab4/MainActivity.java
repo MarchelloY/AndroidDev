211d3401fbb169b2de64ee9bb6dae1c942f92ba5
Marchenko Maxim <maxim.marchenko23@gmail.com>
2019-11-13 15:55:13 +0300

    update 4 lab

--- Lab 4/app/src/main/java/com/marchello/lab4/MainActivity.java
+++ Lab 4/app/src/main/java/com/marchello/lab4/MainActivity.java
@@ -20,14 +20,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button submitButton;
     private TextView amountPrimeNumbers;
     private TextView listPrimeNumbers;
+    private String saveAmount;
+    private String saveList;
 
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @SuppressLint("SetTextI18n")
         @Override
         public void onReceive(Context context, Intent intent) {
-            amountPrimeNumbers.setText(String
-                    .valueOf(intent.getIntExtra("amountPrimeNumbers", 0)));
-            listPrimeNumbers.setText(intent.getStringExtra("listPrimeNumbers"));
+            String amount = context.getResources().getString(R.string.primeNumbers) + " "
+                    + intent.getIntExtra("amountPrimeNumbers", 0) + " "
+                    + context.getResources().getString(R.string.pieces);
+            String list = context.getResources().getString(R.string.listOfNumbers)
+                    + intent.getStringExtra("listPrimeNumbers");
+            amountPrimeNumbers.setText(amount);
+            listPrimeNumbers.setText(list);
+            saveAmount = amount;
+            saveList = list;
             submitButton.setEnabled(true);
         }
     };
@@ -36,13 +44,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LocalBroadcastManager
-                .getInstance(this)
-                .registerReceiver(this.broadcastReceiver, new IntentFilter("filter"));
+        LocalBroadcastManager.getInstance(this).registerReceiver(this.broadcastReceiver, new IntentFilter("filter"));
         this.inputNumber = findViewById(R.id.inputNumber);
         this.submitButton = findViewById(R.id.submitButton);
         this.amountPrimeNumbers = findViewById(R.id.amountPrimeNumbers);
         this.listPrimeNumbers = findViewById(R.id.listPrimeNumbers);
+        if (savedInstanceState != null) {
+            String dataAmound = savedInstanceState.getString("SaveAmount");
+            String dataList = savedInstanceState.getString("SaveList");
+            this.amountPrimeNumbers.setText(dataAmound);
+            this.listPrimeNumbers.setText(dataList);
+            this.saveAmount = dataAmound;
+            this.saveList = dataList;
+        }
         this.submitButton.setOnClickListener(this);
     }
 
@@ -52,5 +66,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         intent.putExtra("number", Integer.valueOf(this.inputNumber.getText().toString()));
         this.submitButton.setEnabled(false);
         startService(intent);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putString("SaveAmount", this.saveAmount);
+        savedInstanceState.putString("SaveList", this.saveList);
+        super.onSaveInstanceState(savedInstanceState);
     }
 }
